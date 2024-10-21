1d9306b58e03ed36ff04b92ff97fc57953d448b4
Yash Gori <yashnileshgori@gmail.com>
2024-10-21 14:32:26 +0530

    first

---
+++ Python.py
@@ -0,0 +1,14 @@
+# +-*(/ division with decimal)
+# (//division without decimal) 
+# (% division remainder)
+# (5**3   five to the power 3)
+# == != > < >= <=
+# and or in middle ,not at start
+# Comma when u use 2 variables, sata types etc
+
+icebag = 1.25
+profit_margin = 0.2
+profit_per_bag = icebag * profit_margin
+no_of_bags = 500
+
+print(profit_per_bag * no_of_bags) 

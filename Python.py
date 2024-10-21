719b7dfb67f8d1a8d442b15ee29106bf0172869a
Yash Gori <yashnileshgori@gmail.com>
2024-10-21 15:13:35 +0530

    two

--- Python.py
+++ Python.py
@@ -2,12 +2,18 @@
 # (//division without decimal) 
 # (% division remainder)
 # (5**3   five to the power 3)
-# == != > < >= <=
+# ==, !=, >, < ,>= ,<=
 # and or in middle ,not at start
-# Comma when u use 2 variables, sata types etc
+# Comma when u use 2 variables, data types etc
+#variables = anything= Anything (only letters, no spaces, only underscores)
+# print("hello world")
+# print("hello" + "world")
+# print("hello" + " " + "world")
+# print("hello" + " " + "world" + " " + "world")
+# all srtings in must be in ""-
 
 icebag = 1.25
-profit_margin = 0.25
+profit_margin = 0.2
 profit_per_bag = icebag * profit_margin
 no_of_bags = 500
 
